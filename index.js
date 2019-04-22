a8357cef7e5b3c1b8074fd0ff4a1a6c3485acb73
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-04-22 16:15:32 -0400

    Configured server to successfully run the API

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // implement your API here
+const express = require('express');
+
+const server = express();
+
+server.get('/', (req, res) => {
+    res.send('Its working');
+})
+
+server.listen(5000, () => {
+    console.log('\n*** API running on port 5k ***\n')
+})
