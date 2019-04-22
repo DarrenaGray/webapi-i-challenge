998d6eb75cc93308c9ad4b49d6c712950e6220b7
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-04-22 16:32:09 -0400

    Created an endpoint that successfully retrieves users in the database on a GET request

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 // implement your API here
 const express = require('express');
 
+const db = require('./data/db');
+
 const server = express();
 
 server.get('/', (req, res) => {
     res.send('Its working');
+})
+
+server.get('/users', (req, res) => {
+    db
+        .find()
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err,
+                message: "The user information could not be retrieved."
+            })
+        })
 })
 
 server.listen(5000, () => {
